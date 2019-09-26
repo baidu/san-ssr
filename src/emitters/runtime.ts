a3b835dba40bc591d3b1eef9fb3c04fc6a217038
harttle <yangjvn@126.com>
2019-09-26 10:56:37 +0800

    feat: configurable san-php-ssr package name

--- src/emitters/runtime.ts
+++ src/emitters/runtime.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 const runtimeFiles = [
     'underscore.php',
     'san.php',
+    'Ts2Php_Helper.php',
     'component-registry.php'
 ]
 
@@ -15,13 +16,5 @@ export function emitRuntimeInPHP (emitter = new PHPEmitter()) {
         emitter.writeLines(readPHPSource(path))
     }
     emitter.endNamespace()
-
-    // TODO move Ts2Php_Helper into \san\runtime namespace
-    // see: https://github.com/max-team/ts2php/issues/49
-    emitter.beginNamespace()
-    const path = resolve(__dirname, `../../runtime/Ts2Php_Helper.php`)
-    emitter.writeLines(readPHPSource(path))
-    emitter.endNamespace()
-
     return emitter.fullText()
 }

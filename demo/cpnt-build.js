1b675604c8971d01d64a58264afe92e6c0e0d8e7
harttle <yangjvn@126.com>
2019-12-25 19:11:29 +0800

    doc: scripts and docs for demo/

--- demo/cpnt-build.js
+++ demo/cpnt-build.js
@@ -1,6 +1,6 @@
 const { SanProject } = require('san-ssr')
 const { writeFileSync } = require('fs')
-const componentClass = require('./src/index.ts')
+const componentClass = require('./src/index.ts').default
 
 const project = new SanProject()
 const targetCode = project.compile(

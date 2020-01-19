b4584ca534d8309ea18c402629dc96315f01f381
harttle <yangjvn@126.com>
2020-01-19 16:22:05 +0800

    chore: sourcemap support for debug usage

--- bin/render-by-source.js
+++ bin/render-by-source.js
@@ -1,23 +1,9 @@
 #!/usr/bin/env node
 
-const { existsSync, readFileSync } = require('fs')
-const { resolve, join } = require('path')
+require('source-map-support/register')
+const { renderBySource } = require('../dist/fixtures/case')
 
 const caseName = process.argv[2]
-const caseRoot = resolve(__dirname, '../test/cases')
-const caseDir = resolve(caseRoot, caseName)
-const jsSSRPath = join(caseDir, 'ssr.js')
+const html = renderBySource(caseName)
 
-const data = getData(caseDir)
-const noDataOutput = /-ndo$/.test(caseDir)
-const jsRendered = require(jsSSRPath)(data, noDataOutput)
-process.stdout.write(jsRendered)
-
-function getData (caseDir) {
-    const dataJSPath = join(caseDir, 'data.js')
-    if (existsSync(dataJSPath)) {
-        return require(dataJSPath)
-    }
-    const dataPath = join(caseDir, 'data.json')
-    return JSON.parse(readFileSync(dataPath, 'utf8'))
-}
+process.stdout.write(html)
--- bin/render-onthefly.js
+++ bin/render-onthefly.js
@@ -1,31 +1,8 @@
 #!/usr/bin/env node
 
-const { existsSync, readFileSync } = require('fs')
-const { resolve, join } = require('path')
-
+require('source-map-support/register')
+const { renderOnthefly } = require('../dist/fixtures/case')
 const caseName = process.argv[2]
-const caseRoot = resolve(__dirname, '../test/cases')
-const caseDir = resolve(caseRoot, caseName)
 
-const render = getRender()
-const data = getData()
-const noDataOutput = /-ndo$/.test(caseName)
-
-const html = render(data, noDataOutput)
+const html = renderOnthefly(caseName)
 process.stdout.write(html)
-
-function getData () {
-    const dataJSPath = join(caseDir, 'data.js')
-    if (existsSync(dataJSPath)) {
-        return require(dataJSPath)
-    }
-    const dataPath = join(caseDir, 'data.json')
-    return JSON.parse(readFileSync(dataPath, 'utf8'))
-}
-
-function getRender () {
-    const ComponentClass = require(join(caseDir, 'component.js'))
-    return require('../dist/index.js').compileToRenderer(ComponentClass, {
-        noTemplateOutput: caseDir.indexOf('notpl') > -1
-    })
-}

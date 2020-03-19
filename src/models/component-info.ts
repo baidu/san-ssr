440e48fa25b1df08681943ef71e5d181368c4a44
harttle <yangjvn@126.com>
2020-03-19 19:16:30 +0800

    fix: Component#getComponentType() not working

--- src/models/component-info.ts
+++ src/models/component-info.ts
@@ -1,4 +1,3 @@
-import { noop } from 'lodash'
 import { ComponentConstructor } from 'san'
 import { CompiledComponent } from './compiled-component'
 import { Filters, Computed } from './component'
@@ -6,6 +5,7 @@ import { Components, ComponentClass } from '../models/component'
 
 interface ComponentInfoOptions {
     filters: Filters
+    component: CompiledComponent<{}>,
     computed: Computed
     template?: string
     cid: number
@@ -33,8 +33,10 @@ export class ComponentInfo {
     public readonly children: ComponentInfo[]
     // Raw components
     public readonly childComponentClasses: Components
+    public component: CompiledComponent<{}>
 
-    constructor ({ filters, computed, template, cid, componentClass, children = [], childComponentClasses }: ComponentInfoOptions) {
+    constructor ({ filters, computed, template, cid, componentClass, children = [], childComponentClasses, component }: ComponentInfoOptions) {
+        this.component = component
         this.filters = filters
         this.computed = computed
         this.template = template
@@ -42,20 +44,5 @@ export class ComponentInfo {
         this.componentClass = componentClass
         this.children = children
         this.childComponentClasses = childComponentClasses
-    }
-
-    createComponentInstance (): CompiledComponent<{}> {
-        // TODO Do not `new Component` during SSR,
-        // see https://github.com/baidu/san-ssr/issues/42
-        const ComponentClass = this.componentClass
-        const proto = ComponentClass.prototype['__proto__']    // eslint-disable-line
-        const calcComputed = proto['_calcComputed']
-        const inited = ComponentClass.prototype['inited']
-        proto['_calcComputed'] = noop
-        ComponentClass.prototype['inited'] = undefined
-        const instance = new ComponentClass()
-        ComponentClass.prototype['inited'] = inited
-        proto['_calcComputed'] = calcComputed
-        return instance as CompiledComponent<{}>
     }
 }
--- src/parsers/component-parser.ts
+++ src/parsers/component-parser.ts
@@ -1,8 +1,9 @@
-import { inspect } from 'util'
-import { isNumber, keys, isFunction } from 'lodash'
+import { ComponentConstructor, ANode } from 'san'
+import { noop, isNumber, keys, isFunction } from 'lodash'
+import { CompiledComponent } from '../models/compiled-component'
 import { ComponentInfo } from '../models/component-info'
 import { getMember } from '../utils/lang'
-import { Components, ComponentClass, Filters, Computed, isComponentClass } from '../models/component'
+import { Components, ComponentClass, Filters, Computed } from '../models/component'
 
 /**
 * 组件解析器，创建并维护组件树
@@ -11,19 +12,54 @@ export class ComponentParser {
     private id = 1
 
     parseComponent (componentClass: ComponentClass): ComponentInfo {
-        if (!isComponentClass(componentClass)) {
-            throw new Error(`${inspect(componentClass)} is not likely a San Component`)
-        }
         const filters = this.parseFilters(getMember(componentClass, 'filters', {}))
+        const component = this.createComponentInstance(componentClass)
         const computed = this.parseComputed(getMember(componentClass, 'computed', {}))
         const template = getMember(componentClass, 'template', '')
-        const childComponentClasses: Components = getMember<Components>(componentClass, 'components', {})
+        const childComponentClasses = this.parseChildComponentClasses(componentClass, component)
         const cid = isNumber(componentClass['sanssrCid']) ? componentClass['sanssrCid'] : this.id
         this.id = Math.max(cid, this.id) + 1
 
         return new ComponentInfo({
-            filters, computed, template, componentClass, cid, childComponentClasses
+            filters, computed, template, componentClass, cid, childComponentClasses, component
         })
+    }
+
+    private parseChildComponentClasses (componentClass: ComponentClass, component: CompiledComponent<{}>): Components {
+        const children: Components = new Map()
+
+        const components: { [key: string]: ComponentConstructor<{}, {}> } = getMember(componentClass, 'components', {})
+        for (const [tagName, componentClass] of Object.entries(components)) {
+            children.set(tagName, componentClass)
+        }
+
+        if (typeof component.getComponentType === 'function') {
+            this.visitANodeRecursively(component.aNode, (aNode: ANode) => {
+                const childClazz = component.getComponentType!(aNode)
+                if (childClazz) children.set(aNode, childClazz)
+            })
+        }
+        return children
+    }
+
+    private visitANodeRecursively (aNode: ANode, visitor: (aNode: ANode) => void) {
+        visitor(aNode)
+        for (const child of aNode.children || []) this.visitANodeRecursively(child, visitor)
+    }
+
+    private createComponentInstance (componentClass: ComponentClass): CompiledComponent<{}> {
+        // TODO Do not `new Component` during SSR,
+        // see https://github.com/baidu/san-ssr/issues/42
+        const ComponentClass = componentClass
+        const proto = ComponentClass.prototype['__proto__']    // eslint-disable-line
+        const calcComputed = proto['_calcComputed']
+        const inited = ComponentClass.prototype['inited']
+        proto['_calcComputed'] = noop
+        ComponentClass.prototype['inited'] = undefined
+        const instance = new ComponentClass()
+        ComponentClass.prototype['inited'] = inited
+        proto['_calcComputed'] = calcComputed
+        return instance as CompiledComponent<{}>
     }
 
     private parseFilters (filters: any): Filters {

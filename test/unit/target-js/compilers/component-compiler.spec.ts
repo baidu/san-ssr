88e341459e2afc60626633707c2959ca8bfd6066
meixg <meixg@foxmail.com>
2021-06-21 15:42:55 +0800

    feat: class-extend unit test

--- test/unit/target-js/compilers/component-compiler.spec.ts
+++ test/unit/target-js/compilers/component-compiler.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentClassCompiler } from '../../../../src/target-js/compilers/component-compiler'
-import { defineComponent } from 'san'
+import { Component, defineComponent } from 'san'
 import { ComponentClassParser } from '../../../../src/parsers/component-class-parser'
 
 describe('target-js/compilers/renderer-compiler', () => {
@@ -9,13 +9,31 @@ describe('target-js/compilers/renderer-compiler', () => {
                 template: '<div></div>',
                 foo: [1, x => x]
             })
-            const sourceFile = new ComponentClassParser(ComponentClass).parse()
+            const sourceFile = new ComponentClassParser(ComponentClass, '').parse()
             const compiler = new ComponentClassCompiler()
             compiler.compile(sourceFile.componentInfos[0])
             expect(compiler.emitter.fullText()).toEqual(`foo: [
     1,
     x => x
 ],
+`)
+        })
+
+        it ('should emit class extends properties', () => {
+            class Middle extends Component {
+                a() {
+                    return 123;
+                }
+            }
+            class MyComponent extends Middle {
+                template = `<div>{{ b }}</div>`
+            }
+            const sourceFile = new ComponentClassParser(MyComponent, '').parse()
+            const compiler = new ComponentClassCompiler()
+            compiler.compile(sourceFile.componentInfos[0])
+            expect(compiler.emitter.fullText()).toEqual(`a: function a() {
+  return 123;
+},
 `)
         })
     })

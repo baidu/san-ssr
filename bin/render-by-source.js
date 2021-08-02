f293583948fac60f19af52e4af67eac70a62f9cb
meixg <meixg@foxmail.com>
2021-08-02 15:03:19 +0800

    test: e2e 测试支持多文件夹

--- bin/render-by-source.js
+++ bin/render-by-source.js
@@ -2,10 +2,12 @@
 
 require('source-map-support/register')
 const { join } = require('path')
-const { caseRoot, getRenderArguments } = require('../dist/fixtures/case')
+const { getRenderArguments, ls } = require('../dist/fixtures/case')
 
 const caseName = process.argv[2]
+const caseItem = ls().find(item => item.caseName === caseName)
+const caseRoot = caseItem.caseRoot
 const render = require(join(caseRoot, `${caseName}/ssr.js`))
-const html = render(...getRenderArguments(caseName))
+const html = render(...getRenderArguments(caseName, caseRoot))
 
 process.stdout.write(html)

a663e93262e234d6de08974aaed5ebfebc0e7d90
meixg <meixg@foxmail.com>
2021-08-02 18:59:42 +0800

    test: 测试产物输出到每个 case 的 output 目录下

--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -1,6 +1,7 @@
 import { ls, compileComponent, compileJS, jsExists, tsExists, compileTS, getRenderArguments, readExpected, renderOnthefly } from '../src/fixtures/case'
 import { join } from 'path'
 import { parseSanHTML } from '../src/index'
+import { existsSync } from 'fs'
 
 for (const { caseName, caseRoot } of ls()) {
     const [expectedData, expectedHtml] = parseSanHTML(readExpected(caseName, caseRoot))
@@ -8,7 +9,7 @@ for (const { caseName, caseRoot } of ls()) {
     if (tsExists(caseName, caseRoot)) {
         it('render to source (TypeScript): ' + caseName, async function () {
             compileTS(caseName, caseRoot)
-            const render = require(join(caseRoot, caseName, 'ssr.js'))
+            const render = require(join(caseRoot, caseName, 'output', 'ssr.js'))
             const got = render(...getRenderArguments(caseName, caseRoot))
             const [data, html] = parseSanHTML(got)
 
@@ -19,11 +20,14 @@ for (const { caseName, caseRoot } of ls()) {
 
     if (jsExists(caseName, caseRoot)) {
         it('js to source: ' + caseName, async function () {
-            const code = compileJS(caseName, caseRoot, true)
-            // eslint-disable-next-line
-            const render = new Function('data', 'noDataOutput', 'require', code)
+            compileJS(caseName, caseRoot)
+            const render = require(join(caseRoot, caseName, 'output/ssr.js'))
+            const ssrSpecPath = join(caseRoot, `${caseName}/ssr-spec.js`)
+            if (existsSync(ssrSpecPath)) {
+                require(ssrSpecPath)
+            }
             // 测试在 strict mode，因此需要手动传入 require
-            const got = render(...getRenderArguments(caseName, caseRoot), require)
+            const got = render(...getRenderArguments(caseName, caseRoot))
             const [data, html] = parseSanHTML(got)
 
             expect(data).toEqual(expectedData)
@@ -31,11 +35,11 @@ for (const { caseName, caseRoot } of ls()) {
         })
 
         it('component to source: ' + caseName, async function () {
-            const code = compileComponent(caseName, caseRoot, true)
+            compileComponent(caseName, caseRoot)
             // eslint-disable-next-line
-            const render = new Function('data', 'noDataOutput', 'require', code)
+            const render = require(join(caseRoot, caseName, 'output/ssr.js'))
             // 测试在 strict mode，因此需要手动传入 require
-            const got = render(...getRenderArguments(caseName, caseRoot), require)
+            const got = render(...getRenderArguments(caseName, caseRoot))
             const [data, html] = parseSanHTML(got)
 
             expect(data).toEqual(expectedData)

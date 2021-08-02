4b7a5d92374356f919026cdbdc47fa6624e5154a
meixg <meixg@foxmail.com>
2021-08-02 21:44:25 +0800

    feat: e2e 测试时，输出到每个case中output下的随机目录中

--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -1,15 +1,22 @@
-import { ls, compileComponent, compileJS, jsExists, tsExists, compileTS, getRenderArguments, readExpected, renderOnthefly } from '../src/fixtures/case'
+import { ls, compileComponent, compileJS, jsExists, tsExists, compileTS, getRenderArguments, readExpected, renderOnthefly, caseRoots } from '../src/fixtures/case'
 import { join } from 'path'
 import { parseSanHTML } from '../src/index'
 import { existsSync } from 'fs'
+import { execSync } from 'child_process'
+
+// 每次执行前，把之前的产物删掉
+caseRoots.forEach(caseRoot => {
+    execSync(`rm -rf ${caseRoot}/**/output`)
+})
 
 for (const { caseName, caseRoot } of ls()) {
     const [expectedData, expectedHtml] = parseSanHTML(readExpected(caseName, caseRoot))
 
     if (tsExists(caseName, caseRoot)) {
         it('render to source (TypeScript): ' + caseName, async function () {
-            compileTS(caseName, caseRoot)
-            const render = require(join(caseRoot, caseName, 'output', 'ssr.js'))
+            const folderName = getRandomStr() + '_tssrc'
+            compileTS(caseName, caseRoot, folderName)
+            const render = require(join(caseRoot, caseName, 'output', folderName, 'ssr.js'))
             const got = render(...getRenderArguments(caseName, caseRoot))
             const [data, html] = parseSanHTML(got)
 
@@ -20,8 +27,9 @@ for (const { caseName, caseRoot } of ls()) {
 
     if (jsExists(caseName, caseRoot)) {
         it('js to source: ' + caseName, async function () {
-            compileJS(caseName, caseRoot)
-            const render = require(join(caseRoot, caseName, 'output/ssr.js'))
+            const folderName = getRandomStr() + '_jssrc'
+            compileJS(caseName, caseRoot, false, folderName)
+            const render = require(join(caseRoot, caseName, 'output', folderName, 'ssr.js'))
             const ssrSpecPath = join(caseRoot, `${caseName}/ssr-spec.js`)
             if (existsSync(ssrSpecPath)) {
                 require(ssrSpecPath)
@@ -35,9 +43,10 @@ for (const { caseName, caseRoot } of ls()) {
         })
 
         it('component to source: ' + caseName, async function () {
-            compileComponent(caseName, caseRoot)
+            const folderName = getRandomStr() + '_comsrc'
+            compileComponent(caseName, caseRoot, false, folderName)
             // eslint-disable-next-line
-            const render = require(join(caseRoot, caseName, 'output/ssr.js'))
+            const render = require(join(caseRoot, caseName, 'output', folderName, 'ssr.js'))
             // 测试在 strict mode，因此需要手动传入 require
             const got = render(...getRenderArguments(caseName, caseRoot))
             const [data, html] = parseSanHTML(got)
@@ -54,4 +63,8 @@ for (const { caseName, caseRoot } of ls()) {
             expect(html).toEqual(expectedHtml)
         })
     }
+}
+
+function getRandomStr () {
+    return Math.random().toString(36).slice(2)
 }

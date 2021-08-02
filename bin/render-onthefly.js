be0b8df50c38b00e57a252d414474bc5ece847f2
meixg <meixg@foxmail.com>
2021-08-02 15:03:19 +0800

    test: e2e 测试支持多文件夹

--- bin/render-onthefly.js
+++ bin/render-onthefly.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 require('source-map-support/register')
-const { renderOnthefly } = require('../dist/fixtures/case')
+const { renderOnthefly, ls } = require('../dist/fixtures/case')
 const caseName = process.argv[2]
+const caseItem = ls().find(item => item.caseName === caseName)
+const caseRoot = caseItem.caseRoot
 
-const html = renderOnthefly(caseName)
+const html = renderOnthefly(caseName, caseRoot)
 process.stdout.write(html)

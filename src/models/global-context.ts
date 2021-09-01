3ac1d06d3ac4ec3bd759fce451aaa9147c5abddf
meixg <meixg@foxmail.com>
2021-09-01 20:12:26 +0800

    test: 补充测试 case provide-class

--- src/models/global-context.ts
+++ src/models/global-context.ts
@@ -1,4 +1,6 @@
+import type { ComponentClass } from './component'
+
 export interface GlobalContext {
     customSSRFilePath?(path: string): string
-    customComponentFilePath?(info: {specifier: string; id: string;}): string
+    customComponentFilePath?(info: {specifier: string; id: string;}): ComponentClass | void
 }

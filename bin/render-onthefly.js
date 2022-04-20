7f3e4518e2a40d9638cc6427578da0afb6601e0d
meixg <meixg@foxmail.com>
2022-04-20 20:19:30 +0800

    test: support custom info param in comrdr

--- bin/render-onthefly.js
+++ bin/render-onthefly.js
@@ -2,12 +2,20 @@
 
 require('source-map-support/register')
 const { renderOnthefly, ls } = require('../dist/fixtures/case')
+const { join } = require('path')
+const fs = require('fs')
 
 function renderOnTheFly (caseName) {
     const caseItem = ls().find(item => item.caseName === caseName)
     const caseRoot = caseItem.caseRoot
 
-    const html = renderOnthefly(caseName, caseRoot)
+    const ssrSpecPath = join(caseRoot, `${caseName}/ssr-spec.js`)
+    let ssrSpec
+    if (fs.existsSync(ssrSpecPath)) {
+        ssrSpec = require(ssrSpecPath)
+    }
+
+    const html = renderOnthefly(caseName, caseRoot, ssrSpec.info || {})
     return html
 }
 

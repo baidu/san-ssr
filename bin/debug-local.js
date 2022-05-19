9c3a4a5cf2d8d7ff930808295a5bfb9886e8dc34
meixg <meixg@foxmail.com>
2022-05-19 17:23:19 +0800

    chore: fix lint

--- bin/debug-local.js
+++ bin/debug-local.js
@@ -3,33 +3,33 @@ const fs = require('fs')
 const path = require('path')
 
 // component to source
-// function compile (componentPath, externalPath, outputPath) {
-//     markExternalComponent({
-//         isExternalComponent (id) {
-//             if (id === externalPath) {
-//                 return true
-//             }
-//         }
-//     })
+function compile (componentPath, externalPath, outputPath) {
+    markExternalComponent({
+        isExternalComponent (id) {
+            if (id === externalPath) {
+                return true
+            }
+        }
+    })
 
-//     // offline
-//     const MyComponent = require(componentPath)
-//     const sanProject = new SanProject()
-//     const res = sanProject.compileToSource(MyComponent, 'js', {
-//         useProvidedComponentClass: true
-//     })
+    // offline
+    const MyComponent = require(componentPath)
+    const sanProject = new SanProject()
+    const res = sanProject.compileToSource(MyComponent, 'js', {
+        useProvidedComponentClass: true
+    })
 
-//     cancelMarkExternalComponent()
+    cancelMarkExternalComponent()
 
-//     fs.writeFileSync(path.resolve(__dirname, outputPath), res)
-// }
+    fs.writeFileSync(path.resolve(__dirname, outputPath), res)
+}
 
 // js to source
-function compile (componentPath, _, outputPath) {
-    const sanProject = new SanProject()
-    const res = sanProject.compileToSource(path.resolve(__dirname, componentPath), 'js')
-    fs.writeFileSync(path.resolve(__dirname, outputPath), res);
-}
+// function compile (componentPath, _, outputPath) {
+//     const sanProject = new SanProject()
+//     const res = sanProject.compileToSource(path.resolve(__dirname, componentPath), 'js')
+//     fs.writeFileSync(path.resolve(__dirname, outputPath), res)
+// }
 
 compile('./sample/component.js', './component2', './dist/component.js')
 compile('./sample/component2.js', './component', './dist/component2.js')
